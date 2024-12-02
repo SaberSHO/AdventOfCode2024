ffadb04499af5e595af558f24626316113e91ed7
Nick Gauthier <ngauthier@sabersho.com>
2024-12-02 13:32:29 -0500

    fix: formattings

--- Day2/day2.go
+++ Day2/day2.go
@@ -9,32 +9,32 @@ import "strconv"
 import "math"
 
 func main() {
-	var levels [][]int
+    var levels [][]int
 
 
-	file, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+    file, err := os.Open("input.txt")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
 
-	// Create a reader
-	reader := bufio.NewReader(file)
+    // Create a reader
+    reader := bufio.NewReader(file)
 
-	// Read the file line by line v
-	var i = 0
-	for {
-	
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			break // Reached end of file
-		}
+    // Read the file line by line v
+    var i = 0
+    for {
+    
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            break // Reached end of file
+        }
 
-		lineSlice := strings.Fields(line)
-		//fmt.Println(lineSlice)
-		
-		ints := make([]int, len(lineSlice))
-		for i, s := range lineSlice {
+        lineSlice := strings.Fields(line)
+        //fmt.Println(lineSlice)
+        
+        ints := make([]int, len(lineSlice))
+        for i, s := range lineSlice {
             // convert string to int
             val, err := strconv.Atoi(s)
             if err != nil {
@@ -45,51 +45,50 @@ func main() {
             // ints slice
             ints[i] = val
         }
-
         levels = append(levels,ints)
-		i++
-	}
+        i++
+    }
 
-	var ReportsSafe = 0
-	var ReportsSafePart2 = 0
+    var ReportsSafe = 0
+    var ReportsSafePart2 = 0
 
-	for j := range levels {
-		//check each level for safe/unsafe
-		//First check all increasing or all decreasing
-		fmt.Println(levels[j],"is increasing = ",isSafelyIncreasing(levels[j]))
-		fmt.Println(levels[j],"is decreasing = ",isSafelyDecreasing(levels[j]))
+    for j := range levels {
+        //check each level for safe/unsafe
+        //First check all increasing or all decreasing
+        fmt.Println(levels[j],"is increasing = ",isSafelyIncreasing(levels[j]))
+        fmt.Println(levels[j],"is decreasing = ",isSafelyDecreasing(levels[j]))
 
-		if (isSafelyDecreasing(levels[j]) || isSafelyIncreasing(levels[j])) {
-			ReportsSafe += 1
-		}
-	}
+        if (isSafelyDecreasing(levels[j]) || isSafelyIncreasing(levels[j])) {
+            ReportsSafe += 1
+        }
+    }
 
-	fmt.Println("Safe Reports (part 1): ", ReportsSafe)
+    fmt.Println("Safe Reports (part 1): ", ReportsSafe)
 
-	
-	for j := range levels {
-		//check each level for safe/unsafe
-		//First check all increasing or all decreasing
-		//fmt.Println(levels[j],"is increasing = ",isSafelyIncreasing(levels[j]))
-		//fmt.Println(levels[j],"is decreasing = ",isSafelyDecreasing(levels[j]))
+    
+    for j := range levels {
+        //check each level for safe/unsafe
+        //First check all increasing or all decreasing
+        //fmt.Println(levels[j],"is increasing = ",isSafelyIncreasing(levels[j]))
+        //fmt.Println(levels[j],"is decreasing = ",isSafelyDecreasing(levels[j]))
 
-		if (isSafelyDecreasing(levels[j]) || isSafelyIncreasing(levels[j])) {
-			ReportsSafePart2 += 1
-		} else {
-			//Now we try with Toleration
-			var subTest []int
-			for k := range levels[j] {
-				subTest = RemoveIndex(levels[j],k)
-				//fmt.Println(levels[j],k, subTest)
-				if (isSafelyDecreasing(subTest) || isSafelyIncreasing(subTest)){
-					ReportsSafePart2 +=1
-					break
-				}
-			}
-		}
-	}
+        if (isSafelyDecreasing(levels[j]) || isSafelyIncreasing(levels[j])) {
+            ReportsSafePart2 += 1
+        } else {
+            //Now we try with Toleration
+            var subTest []int
+            for k := range levels[j] {
+                subTest = RemoveIndex(levels[j],k)
+                //fmt.Println(levels[j],k, subTest)
+                if (isSafelyDecreasing(subTest) || isSafelyIncreasing(subTest)){
+                    ReportsSafePart2 +=1
+                    break
+                }
+            }
+        }
+    }
 
-	fmt.Println("Safe Reports (part 2): ", ReportsSafePart2)
+    fmt.Println("Safe Reports (part 2): ", ReportsSafePart2)
 }
 
 func isSafelyIncreasing(s []int) bool {
@@ -106,7 +105,6 @@ func isSafelyDecreasing(s []int) bool {
         if s[i] >= s[i-1] || math.Abs(float64(s[i]-s[i-1])) >3 {
             return false
         }
-
     }
     return true
 }
