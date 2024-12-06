377216543569b6524e71998849ce81ac7708f09d
Nick Gauthier <ngauthier@sabersho.com>
2024-12-06 10:23:47 -0500

    day 6 part 1

---
+++ Day6/day6.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Direction struct {
+	X int
+	Y int
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	//parse rules first into a map
+	var grid [][]string
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			break
+		}
+		line := scanner.Text()
+		grid = append(grid, strings.Split(line, ""))
+	}
+
+	//set up our pathing Points/Directions
+	var curPos Point
+
+	curDir := Direction{0, -1}
+
+	//find starting point
+	curPos = findCharIndex(grid, "^")
+
+	var allVisited []Point
+	for {
+		tryNewPos := moveInDirection(curPos, curDir)
+		if tryNewPos.X < 0 || tryNewPos.Y < 0 || tryNewPos.X >= len(grid[0]) || tryNewPos.Y >= len(grid) {
+			allVisited = append(allVisited, curPos)
+			break
+		}
+		if grid[tryNewPos.Y][tryNewPos.X] == "#" {
+			curDir = nextDir(curDir)
+		}
+		allVisited = append(allVisited, curPos)
+		curPos = moveInDirection(curPos, curDir)
+
+	}
+
+	//we only want unique points
+	visited := map[Point]bool{}
+	for _, v := range allVisited {
+		visited[v] = true
+	}
+
+	fmt.Println("Visited: ", len(visited))
+}
+
+func findCharIndex(matrix [][]string, char string) Point {
+	for row, slice := range matrix {
+		for col, val := range slice {
+			if val == char {
+				p := Point{col, row}
+				return p
+			}
+		}
+	}
+	return Point{0, 0} // Return -1 if character not found
+}
+
+func moveInDirection(current Point, direction Direction) Point {
+	return Point{current.X + direction.X, current.Y + direction.Y}
+}
+
+func nextDir(currentDirection Direction) Direction {
+	up := Direction{0, -1}
+	down := Direction{0, 1}
+	left := Direction{-1, 0}
+	right := Direction{1, 0}
+
+	if currentDirection == up {
+		return right
+	}
+	if currentDirection == right {
+		return down
+	}
+	if currentDirection == down {
+		return left
+	}
+	if currentDirection == left {
+		return up
+	}
+	return Direction{0, 0}
+}
